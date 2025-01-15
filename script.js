a92d75b4311e95209ab6d1945a5aa4f8c1749112
Tarun Bavra <112540230+Tarun-Bavra@users.noreply.github.com>
2025-01-15 23:13:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,21 +3,30 @@ const buttons = document.getElementsByClassName("btn");
 const steps = document.getElementsByClassName("step");
 const lines = document.getElementsByClassName("line");
 const stepsArray = Array.from(steps);
-let step=0; let line=0;
+let step=0; let line=1;
 //prev btn
 buttons[0].addEventListener("click",(e)=>{
-	if(step>0){
-		step = step - 1;line = line - 1;
+	//prev btn
+	if(step>=1){
+		step--;
 	stepsArray[step].style.backgroundColor = "transparent";
+	}
+	//prev line
+	if(line>=1){
+		line--;
 	lines[line].style.backgroundColor = "transparent";
 	}
 });
+
 //next btn
 buttons[1].addEventListener("click",(e)=>{
-	if(step<5){
-	stepsArray[step].style.backgroundColor = "skyblue";
-	lines[line].style.backgroundColor = "skyblue";
-		step = step + 1;line = line + 1;
-	}
-	
-})
+	//next btn
+	if (step < steps.length) {
+        steps[step].style.backgroundColor = "skyblue"; // Highlight current step
+        if (step > 0) {
+            lines[line].style.backgroundColor = "skyblue"; // Highlight connecting line
+            line++; // Move to the next line
+        }
+        step++; // Move to the next step
+    }
+});
