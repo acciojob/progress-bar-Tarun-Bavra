c2e6372d8cb3cb3065a185445553f2b55f9ded75
Tarun Bavra <112540230+Tarun-Bavra@users.noreply.github.com>
2025-01-15 22:27:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 //your JS code here. If required.
 const buttons = document.getElementsByClassName("btn");
 const steps = document.getElementsByClassName("step");
+const lines = document.getElementsByClassName("line");
 const stepsArray = Array.from(steps);
-const [step1,step2,step3,step4,step5] = stepsArray;
-let step=0;
+let step=0; let line=0;
+//prev btn
 buttons[0].addEventListener("click",(e)=>{
 	if(step>0){
+		step = step - 1;line = line - 1;
 	stepsArray[step].style.backgroundColor = "transparent";
-		step = step - 1;
+	lines[line].style.backgroundColor = "transparent";
 	}
-})
+});
+//next btn
 buttons[1].addEventListener("click",(e)=>{
-	if(step<=5){
+	if(step<5){
 	stepsArray[step].style.backgroundColor = "skyblue";
+	lines[line].style.backgroundColor = "skyblue";
+		step = step + 1;line = line + 1;
 	}
-	step = step + 1;
+	
 })
